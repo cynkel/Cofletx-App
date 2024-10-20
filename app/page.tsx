d643b79ad0f8657512546329697d929993dbe733
Kelvin <kelxir@outlook.com>
2024-10-20 01:41:17 +0000

    Revised Design(1)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 import Header from "@/app/components/header";
 import Hero from "@/app/components/hero";
 import Services from "@/app/components/services";
-import FAQ from "@/app/components/faq"; 
-import Card from "./components/card";
-import Image from "next/image";
 
 export default function Home() {
   return (
