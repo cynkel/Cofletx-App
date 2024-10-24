f0d1ce649ae02be9033a6df3750219ac008f36c2
Kelvin <kelxir@outlook.com>
2024-10-24 17:20:09 +0000

    Fix Orientation 2.0

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Hero() {
   return (
     <section className="flex items-center justify-center bg-hero bg-cover h-svh w-full">
-      <div className="flex flex-col items-center gap-4 justify-center h-full w-full bg-black bg-opacity-80 tracking-wide leading-loose">
+      <div className="flex flex-col items-center gap-4 justify-center h-full w-full py-20 bg-black bg-opacity-80 tracking-wide leading-loose">
         <h1 className="font-bold text-2xl md:text-5xl text-white text-center w-2/3">
           <span className="text-yellow">Leading Experts</span> in innovative
           building design <span className="text-purple-900">and</span> project
@@ -23,22 +23,6 @@ export default function Hero() {
             Explore Our Work
           </button>
           </Link>
-        </div>
-        <div className="text-center flex flex-col items-center absolute bottom-24 lg:bottom-8">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-12 md:size-24 lg:size-16 stroke-white animate-bounce"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-            />
-          </svg>
         </div>
       </div>
     </section>
